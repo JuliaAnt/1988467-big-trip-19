e5c514d007ac690b6a858360a5fe7d45431872d4
Julia Antoshkina <julia.antoshkina@gmail.com>
2023-01-16 12:49:35 +0100

    Deleted comments

--- src/model.js
+++ src/model.js
@@ -3,7 +3,6 @@ import { getRandomElement } from './mock/mock-data.js';
 const NUMBER_OF_EVENTS = 3;
 
 export default class PointsModel {
-  // #points = Array.from({ length: NUMBER_OF_EVENTS }, getRandomElement);
   #points = [];
 
   get points() {
